ef44f2bb8e23034145b75ca3bdfcd5d5a28a9502
KAMAbee <kamil.akbarov.94@gmail.com>
2024-04-16 16:55:02 +0500

    Second commit

---
+++ src/Mylist.java
@@ -0,0 +1,21 @@
+public interface Mylist<T> extends Iterable<T>{
+    //Adding a new item in an arraylist
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+    boolean exists(Object object);
+    public Object[] toArray();
+    int size();
+
+}
