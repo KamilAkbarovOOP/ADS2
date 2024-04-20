6fabeff46c71e330317cfb635c756c9f0216e7d3
KAMAbee <kamil.akbarov.94@gmail.com>
2024-04-20 22:18:45 +0500

    3

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,7 @@ import java.sql.Array;
 
 public class Main {
     public static void main(String[] args) {
-        MyLinkedList<Integer> list = new MyLinkedList<>();
-        Object[] arr;
 
-
-        list.add(10);
-        list.add(1);
-        list.add(5);
-        list.add(3);
-        list.add(1, 2);
-        list.sort();
-        arr = list.toArray();
-        for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
-        }
     }
 }
 
