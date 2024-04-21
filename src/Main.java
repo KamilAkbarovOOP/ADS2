7c334e1837ff1c58d1c977d2ef9c5f34ca45da4e
KAMAbee <kamil.akbarov.94@gmail.com>
2024-04-21 16:46:18 +0500

    5

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,6 @@ import java.sql.Array;
 
 public class Main {
     public static void main(String[] args) {
-        MyLinkedList<Integer> list = new MyLinkedList<>();
-        MyQueue <Integer> queue = new MyQueue<>();
-        MyMinHeap <Integer> heap = new MyMinHeap<>();
-
-
-        heap.insert(60);
-        heap.insert(50);
-        heap.insert(21);
-        heap.insert(40);
-        heap.insert(10);
-        heap.insert(51);
-        heap.insert(5);
-        System.out.println(heap.extractMin());
-
 
     }
 }
